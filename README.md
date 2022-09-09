3a4df04dc492868f598f7e7bed33d9de6c923118
muyawia <muyawia_kashif@outlook.com>
2022-09-09 19:00:43 +0100

    complete readme and populate commented backend files

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # 13-Ecommerce-BackEnd
+
+## Description
+Code for an E-commerce shop using MySql for the backend database. There is no front end page to interact with the backend.
+
+## Usage
+
+To use it use the free application Insomnia which can be downloaded here: https://insomnia.rest/download
+
+The application's GET routes to return all categories, products, and tags being tested:
+
+![In Insomnia, the user tests “GET tags,” “GET Categories,” and “GET All Products.”.](./Assets/13-orm-homework-demo-01.gif)
+
+The following animation shows the application's GET routes to return a single category, a single product, and a single tag being tested in Insomnia:
+
+![In Insomnia, the user tests “GET tag by id,” “GET Category by ID,” and “GET One Product.”](./Assets/13-orm-homework-demo-02.gif)
+
+The following animation shows the application's POST, PUT, and DELETE routes for categories being tested in Insomnia:
+
+![In Insomnia, the user tests “DELETE Category by ID,” “CREATE Category,” and “UPDATE Category.”](./Assets/13-orm-homework-demo-03.gif)
+
+This application is deployed on GitHub pages.
+
+## TODO:
+
+## BUGS:
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,27 +4,49 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const AllProductData = await Product.findAll({
+      include: [{model: Category}, {model: Tag}],
+    });
+    res.status(200).json(AllProductData);
+  } 
+    catch (err) {
+      res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try {
+    const AllProductData = await Product.findByPk(req.params.id,{
+      include: [{model: Category}, {model: Tag}],
+    });
+    res.status(200).json(AllProductData);
+  } 
+    catch (err) {
+      res.status(500).json(err);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
+router.post('/', async (req, res) => {
+  try {
+    const AllCategoryData = await Category.create({
+  products: {
       product_name: "Basketball",
       price: 200.00,
       stock: 3,
       tagIds: [1, 2, 3, 4]
     }
-  */
+  });
+  res.status(200).json(AllCategoryData);
+}   
+  catch (err) {
+    res.status(400).json(err);
+}
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
